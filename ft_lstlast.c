e72c8c5abb48b3766fc7787ae1c4d7f25f6524b0
Jose Pedro Bandeira Gomes <jpedro-b@c1r15s4.42porto.com>
2025-04-16 16:58:57 +0100

    Cleaned and ft_lstlast done

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: jpedro-b <jpedro-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 15:52:31 by jpedro-b          #+#    #+#             */
-/*   Updated: 2025/04/16 16:25:10 by jpedro-b         ###   ########.fr       */
+/*   Updated: 2025/04/16 16:57:21 by jpedro-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,10 @@ t_list	*ft_lstlast(t_list *lst)
 {
 	t_list	*tmp;
 
+	if (!lst)
+		return (NULL);
 	tmp = lst;
-	printf("\n%p\n", tmp);
-	while (tmp)
-	{
-		printf("\n%p\n", tmp);
+	while (tmp->next)
 		tmp = tmp->next;
-	}
 	return (tmp);
 }
